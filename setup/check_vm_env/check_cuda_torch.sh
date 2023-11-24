62e2efa806d075a147507daa2dbd080f16be8004
renya <rsmilek5492@gmail.com>
2023-11-24 21:48:45 +0900

    add comment

--- setup/check_vm_env/check_cuda_torch.sh
+++ setup/check_vm_env/check_cuda_torch.sh
@@ -10,5 +10,6 @@ nvidia-smi
 
 # check torch
 echo "==============check torch=============="
+echo "if you exec at first time, you might wait for a while..."
 python -c "import torch; print(f'torch.__version__: {torch.__version__}')"
 python -c "import torch; print(f'torch.cuda.is_available(): {torch.cuda.is_available()}')"
