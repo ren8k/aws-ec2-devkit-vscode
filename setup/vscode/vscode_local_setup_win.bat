7905baf139bd089a5f9fb083eb79c67e13a3367d
renya <rsmilek5492@gmail.com>
2024-03-30 03:48:50 +0000

    Add &^ to exec commands continuously

--- setup/vscode/vscode_local_setup_win.bat
+++ setup/vscode/vscode_local_setup_win.bat
@@ -2,12 +2,12 @@
 setlocal enabledelayedexpansion
 cd %~dp0
 
-@REM set git credential helper (you need to install git)
+@REM set git credential helper (if you need to clone repos from aws codecommit)
 @REM git config --global credential.helper "!aws codecommit credential-helper $@"
 @REM git config --global credential.UseHttpPath true
 
 @REM  install vscode extension
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension amazonwebservices.aws-toolkit-vscode
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack &^
+code --install-extension amazonwebservices.aws-toolkit-vscode &^
 code --install-extension pengzhanzhao.ec2-farm
 echo finish
