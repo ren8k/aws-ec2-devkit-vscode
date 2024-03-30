5d72b124af5be3809202b96a6daa08682b157fc3
renya <rsmilek5492@gmail.com>
2024-03-30 04:01:56 +0000

    Add project manager extension to vscode_local_setup_win.bat

--- setup/vscode/vscode_local_setup_win.bat
+++ setup/vscode/vscode_local_setup_win.bat
@@ -9,5 +9,6 @@ cd %~dp0
 @REM  install vscode extension
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack &^
 code --install-extension amazonwebservices.aws-toolkit-vscode &^
+code --install-extension alefragnani.project-manager &^
 code --install-extension pengzhanzhao.ec2-farm
 echo finish
